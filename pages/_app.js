54432c7663d13711bbb4144dd3711d556d300d86
sanchia-menezes <sanchia.menezes19@gmail.com>
2022-12-18 16:26:52 -0500

    Addition of products, product details, product recommendations and layout features

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>)
 }
